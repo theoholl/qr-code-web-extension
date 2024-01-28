222f3c6f866a221e360a216bef87b2b90e5c2e31
Theo Hollweg <thollweg@mailbox.org>
2024-01-28 17:31:18 +0100

    Implement basic design

---
+++ popup.js
@@ -0,0 +1,41 @@
+function handleClickCloseWindowButton() {
+    window.close();
+}
+
+function handleClickCopyUrlButton() {
+    const urlInput = document.getElementById("url-input");
+    navigator.clipboard.writeText(urlInput.value);
+}
+
+async function getCurrentTabUrl() {
+    const tabs = await browser.tabs.query({ active: true });
+    const url = tabs[0].url ?? "";
+    
+    return url;
+}
+
+function updateQrCode() {
+    const urlInput = document.getElementById("url-input");
+    const qrCode = document.getElementById("qr-code");
+    const oldHtmlTable = document.getElementsByClassName("qrcode")[0];
+    const newhtmlTable = QRCode.generateHTML(urlInput.value);
+    qrCode.replaceChild(newhtmlTable, oldHtmlTable);
+}
+
+async function initialize() {
+    const closeButton = document.getElementById("close-button");
+    closeButton.onclick = handleClickCloseWindowButton;
+
+    const copyButton = document.getElementById("copy-button");
+    copyButton.onclick = handleClickCopyUrlButton;
+
+    const urlInput = document.getElementById("url-input");
+    urlInput.value = await getCurrentTabUrl();
+    urlInput.addEventListener("change", updateQrCode);
+
+    const qrCode = document.getElementById("qr-code");
+    const htmlTable = QRCode.generateHTML(urlInput.value);
+    qrCode.appendChild(htmlTable);
+}
+
+initialize();
